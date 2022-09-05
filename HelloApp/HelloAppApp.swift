dc2c31386486ea176a79eefa4a17418504107464
KohutiakMaksym <makskogutiak@gmail.com>
2022-09-06 01:08:31 +0200

    Initial Commit

---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by MAKS on 06.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
