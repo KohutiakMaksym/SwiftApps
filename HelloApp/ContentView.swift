3ccc6caff6a19d5144d58d94c234781d45b58d09
KohutiakMaksym <makskogutiak@gmail.com>
2022-09-08 00:50:41 +0200

    Added custom colours and different days forecast

--- HelloApp/ContentView.swift
+++ HelloApp/ContentView.swift
@@ -10,13 +10,46 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(
+                colors: [.blue, Color("LightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             VStack{
                 Text("Lodz, Poland")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                VStack(spacing: 8) {
+                    Image(systemName: "sunset.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("54°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }.padding(.bottom, 50)
+                HStack(spacing: 15){
+                    WeatherDayView(
+                        dayofWeek: "TUE",
+                        imageName: "cloud.sun.fill",
+                        temprature: 56)
+                    WeatherDayView(
+                        dayofWeek: "WED",
+                        imageName: "sun.max.fill",
+                        temprature: 66)
+                    WeatherDayView(
+                        dayofWeek: "THU",
+                        imageName: "wind.snow",
+                        temprature: 43)
+                    WeatherDayView(
+                        dayofWeek: "FRI",
+                        imageName: "snow",
+                        temprature: 20)
+                    WeatherDayView(
+                        dayofWeek: "SAT",
+                        imageName: "snow",
+                        temprature: 14)
+                }
                 Spacer()
             }
         }
@@ -26,5 +59,31 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayofWeek: String
+    var imageName: String
+    var temprature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayofWeek)
+                .font(.system(size: 15, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .padding(5)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .padding(5)
+            Text("\(temprature)°")
+                .font(.system(size: 19, weight: .bold, design: .default))
+                .foregroundColor(.white)
+                .padding(5)
+        }
     }
 }
